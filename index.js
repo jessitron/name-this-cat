816655a909a8bc25fa6f26d217d8f0ed1977f83e
Jessica Kerr <jessitron@gmail.com>
2020-03-14 16:34:32 -0500

    It now redirects and puts the image in based on the parameter

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ app.use(express.urlencoded());
 
 app.post('/name', (req, res) =>
 
-    res.send('The cat will be named: ' + req.body.name))
+    res.redirect("/?picture=" + req.body.name)
+)
 
 // goal:
 // convert images/pixie.jpg -gravity south -stroke '#000C' -strokewidth 2 -pointsize 100 -font StayPuft -annotate 0 "Pixie" -stroke none -fill white -annotate 0 "Pixie"  output-images/pixie.jpg
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
