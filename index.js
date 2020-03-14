3d02a7b40ee13abb142dcd767e4597c4f319b5aa
Jessica Kerr <jessitron@gmail.com>
2020-03-13 19:27:00 -0500

    I can post a cat name

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ const port = 3000
 // app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use(express.static('public'))
+app.use(express.urlencoded());
+
+app.post('/name', (req, res) =>
+
+    res.send('The cat will be named: ' + req.body.name))
+
+// goal:
+// convert images/pixie.jpg -gravity south -stroke '#000C' -strokewidth 2 -pointsize 100 -font StayPuft -annotate 0 "Pixie" -stroke none -fill white -annotate 0 "Pixie"  output-images/pixie.jpg
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
