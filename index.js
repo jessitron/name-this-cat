da9e1efb95b234153030bef7b5b23f29950e9601
Jessica Kerr <jessitron@gmail.com>
2020-03-14 17:07:14 -0500

    It works!

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ const port = 3000;
 // app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use(express.static('public'))
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 
 app.post('/name', (req, res) => {
     const catName = req.body.name;
     const outputName = hashOfName(catName);
-    // goal:
-    // convert images/pixie.jpg -gravity south -stroke '#000C' -strokewidth 2 -pointsize 100 -font StayPuft -annotate 0 "Pixie" -stroke none -fill white -annotate 0 "Pixie"  output-images/pixie.jpg
-
-    res.redirect("/?picture=" + outputName);
+    child_process.exec("convert " + convertImageToImageWithText(catName, outputName).join(" "),
+        (err, stdout, stderr) => {
+            console.error(stderr);
+            res.redirect("/?picture=" + outputName);
+        });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
@@ -24,10 +25,11 @@ function hashOfName(catName) {
     return crypto.createHash('SHA1').update(catName).digest('hex');
 }
 
-function generatePictureWithText(catName, outputPath, cb) {
-    const inputPath = "images/pixie.jpg";
+function convertImageToImageWithText(catName, outputName) {
+    const inputPath = "public/images/pixie.jpg";
+    const outputPath = "public/output-images/" + outputName + ".jpg"
     const quotedText = '"' + catName + '"';
-    child_process.exec("convert", [
+    return [
         inputPath,
         "-gravity", "south",
         "-stroke", "'#000C'",
@@ -41,5 +43,5 @@ function generatePictureWithText(catName, outputPath, cb) {
         "-annotate", "0",
         quotedText,
         outputPath
-    ], cb)
+    ];
 }
