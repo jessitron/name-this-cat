c745e2d1030d91be884d13b2a4b72825cab9167c
Jessica Kerr <jessitron@gmail.com>
2020-05-04 12:40:07 -0500

    Here is a version with duplication removed

--- index.js
+++ index.js
@@ -7,25 +7,31 @@ const port = 3000;
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }));
 
-const CHARACTERS_THAT_FIT_ON_THE_IMAGE = 30;
+const CHARACTERS_THAT_FIT_ON_THE_PICTURE = 30;
 
 app.post('/name', (req, res) => {
     const catName = req.body.name;
 
-    if (!/[a-zA-Z]/.test(catName) || catName.length > CHARACTERS_THAT_FIT_ON_THE_IMAGE) {
+    if (!/[a-zA-Z]/.test(catName) || catName.length > CHARACTERS_THAT_FIT_ON_THE_PICTURE) {
         res.status(400).send("I don't like that name");
         return;
     }
 
+    createNamedCatPicture(catName, (err, outputName) => {
+        res.redirect("/?picture=" + outputName);
+    });
+});
+
+function createNamedCatPicture(catName, cb) {
     const outputName = hashOfName(catName);
     const arguments = convertImageToImageWithText(catName, outputName);
     console.log("Running: " + arguments);
     child_process.execFile("convert", arguments,
         (err, stdout, stderr) => {
             console.error(stderr);
-            res.redirect("/?picture=" + outputName);
+            cb(err, outputName);
         });
-});
+}
 
 app.get('/catPicture', (req, res) => {
     const catName = req.query.name;
@@ -34,14 +40,9 @@ app.get('/catPicture', (req, res) => {
         res.status(400).send("Please specify 'name' as a URL parameter")
     }
 
-    const outputName = hashOfName(catName);
-    const arguments = convertImageToImageWithText(catName, outputName);
-    console.log("Running: " + arguments);
-    child_process.execFile("convert", arguments,
-        (err, stdout, stderr) => {
-            console.error(stderr);
-            res.redirect("/output-images/" + outputName + ".jpg");
-        });
+    createNamedCatPicture(catName, (err, outputName) => {
+        res.redirect("/output-images/" + outputName + ".jpg");
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
