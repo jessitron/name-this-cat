8a1b87b4cf26389404faaaf989a3e1ebeb79a659
Jessica Kerr <jessitron@gmail.com>
2020-05-05 16:27:40 -0500

    Tests for catName

---
+++ test/CatNameTest.js
@@ -0,0 +1,21 @@
+let assert = require("assert");
+let CatName = require("../CatName");
+
+describe('CatName', function () {
+  it('requires at least one letter', () => {
+    assert.throws(() => {
+      new CatName("123");
+    });
+  });
+
+  it('does not accept very long strings', () => {
+    assert.throws(() => {
+      new CatName("a234567890123456789012345678");
+    });
+  });
+
+  it('keeps the cat name safe', () => {
+    const pixie = new CatName("Pixie");
+    assert.equal(pixie.text, "Pixie");
+  })
+});
